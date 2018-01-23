d77c4abcda72dbc974064e6eab6449cb5db51c11
Zhirnov Vladimir <vmz1net@gmail.com>
2018-01-23 21:08:56 +0300

    add brain-even

---
+++ src/bin/brain-even.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { hello, answer, task, game } from '..';
+
+hello();
+task();
+answer();
+game();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import readlineSync from 'readline-sync';
 
+let user = '';
+
 export const hello = () => {
-  console.log('Welcome to the Brain Games!');
+  console.log('\nWelcome to the Brain Games!');
 };
+
 export const answer = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi, ${userName}!`);
+  console.log(`Hi, ${userName}!\n`);
+  user = userName;
+};
+
+export const task = () => {
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+};
+
+export const game = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const num = Math.round(Math.random() * ((100 - 1) + 1));
+    console.log(`Question: ${num}`);
+    const choice = readlineSync.question('Your answer: ');
+
+    if ((num % 2 === 0 && choice === 'yes') || (num % 2 !== 0 && choice === 'no')) {
+      console.log('Correct!\n');
+    } else {
+      const isNumEven = () => (num % 2 === 0 ? 'yes' : 'no');
+      return console.log(`'${choice}' is wrong answer. Correct answer was '${isNumEven()}'. Let's try again, ${user}!)\n`);
+    }
+  }
+  return console.log(`Congratulations, ${user}!\n`);
 };
