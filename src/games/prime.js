c91991f5da73c2312175d82b4888e0c87e6c8cda
Zhirnov Vladimir <vmz1net@gmail.com>
2018-01-27 18:26:11 +0300

    changes in  prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,17 +3,17 @@ import random from '../random';
 
 const task = 'Is this number prime?';
 
-const prime = (num) => {
+const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return 'no';
+    if (num % i === 0) return false;
   }
-  return 'yes';
+  return true;
 };
 
 export default () => {
   const data = () => {
     const question = random(1000);
-    const answer = prime(question);
+    const answer = isPrime(question) ? 'yes' : 'no';
     return { question, answer };
   };
   return startGame(data, task);
