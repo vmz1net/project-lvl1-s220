64ef4a24eac068d9d4c0248f752c901549afd2ef
Zhirnov Vladimir <vmz1net@gmail.com>
2018-01-28 08:11:23 +0300

    refactoring progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,34 +3,29 @@ import random from '../random';
 
 const task = 'Balance the given number.';
 
-const progression = (num) => {
-  let x1 = num; 
-  let seriesNumbers = ' ';
-  let seriesNumbersContinue = ' ';
-  const range = random(10);
-  const add = random(10);
 
-  for (let i = 0; i < range - 1; i += 1) {
-    x1 += add;
-    seriesNumbers += `${x1} `;
+const progression = (startDigit, diff, steps, hiddenNumber) => {
+  const arrayOfNumbers = [];
+  let count = 0;
+  while (count < steps) {
+    arrayOfNumbers.push(startDigit + (diff * count));
+    count += 1;
   }
-  let x2 = x1 + add;
-  const numberHide = x1 + add;
-  for (let j = range; j < 10; j += 1) {
-    x2 += add;
-    seriesNumbersContinue += `${x2} `;
-  }
-  const str = `${seriesNumbers} .. ${seriesNumbersContinue}`;
-  return { str, numberHide };
+  const hiddenAnswer = arrayOfNumbers[hiddenNumber];
+  arrayOfNumbers[hiddenNumber] = '..';
+  const question = arrayOfNumbers.join(' ');
+  return { question, hiddenAnswer };
 };
 
 
 export default () => {
   const data = () => {
-    const num = random(100);
-    const { str, numberHide } = progression(num);
-    const question = str;
-    const answer = numberHide;
+    const startDigit = random(100);
+    const diff = random(10);
+    const hiddenNumber = random(10);
+    const steps = 10;
+    const { question, hiddenAnswer } = progression(startDigit, diff, steps, hiddenNumber);
+    const answer = hiddenAnswer;
     return { question, answer };
   };
   return startGame(data, task);
